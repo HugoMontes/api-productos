1aac0b8091fb0267f998abfaf1b1191f32805d0f
hmontes <hmontes@cofar.com.bo>
2020-10-05 16:44:40 -0400

    Agregar elementos a la base de datos

--- database.sql
+++ database.sql
@@ -9,3 +9,9 @@ CREATE TABLE productos(
   imagen varchar(255),
   CONSTRAINT pk_productos PRIMARY KEY(id)
 )ENGINE=InnoDb;
+
+INSERT INTO productos(nombre, descripcion, precio) VALUES('LAPTOP LENOVO','Computadora Lenovo 15,6" AMD RYZEN 5 3500u WINDOWS 10 HOME', 6000);
+INSERT INTO productos(nombre, descripcion, precio) VALUES('LAPTOP HUAWEI','Computadora portátil Huawei de 13" AMD Ryzen5', 7500);
+INSERT INTO productos(nombre, descripcion, precio) VALUES('TELEVISOR SAMSUNG','Televisor Samsung 55" smart tv serie TU8000 UHD 4k', 6200);
+INSERT INTO productos(nombre, descripcion, precio) VALUES('REFRIGERADOR ELECTROLUX','Refrigerador Electrolux semi seco 250 ltrs2 puertas acero', 2600);
+INSERT INTO productos(nombre, descripcion, precio) VALUES('LAVADORA WHIRLPOOL','Lavadora Whirlpool carga superior de 16Kg blanco', 4400);
